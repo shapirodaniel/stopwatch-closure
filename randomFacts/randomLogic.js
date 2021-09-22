becae2ff4b60e381737f2a148d1a90a2da43a46b
shapirodaniel <shapirodanieladam@gmail.com>
2021-09-21 19:34:25 -0500

    add: final randomLogic.js

--- randomFacts/randomLogic.js
+++ randomFacts/randomLogic.js
@@ -1,12 +1,30 @@
-const getRandomFact = () => {
-	const url = 'https://uselessfacts.jsph.pl/random.json?language=en';
-	fetch(url)
-		.then(res => res.json())
-		.then(data => {
-			document.getElementById('message').innerText = data.text;
-		});
+const throttleRequest = (requestFn, threshold) => {
+	let isOkToRequest = true;
+
+	return function (...args) {
+		if (isOkToRequest) {
+			requestFn.apply(this, args);
+
+			isOkToRequest = false;
+
+			setTimeout(() => {
+				isOkToRequest = true;
+			}, threshold);
+		} else {
+			console.log(`Requests must be placed ${threshold}ms apart`);
+		}
+	};
 };
+
+const getRandomFact = async () => {
+	const url = 'https://uselessfacts.jsph.pl/random.json?language=en';
+	const res = await fetch(url);
+	const data = await res.json();
+	document.getElementById('message').innerText = data.text;
+};
+
+const throttledGetRandomFact = throttleRequest(getRandomFact, 1000);
 
 document
 	.getElementById('getFact')
-	.addEventListener('click', () => getRandomFact());
+	.addEventListener('click', () => throttledGetRandomFact());
